79919f6397fae335594f8abdb589378041c70b1b
Дмитрий Шелестовский <dashelesto@mts.ru>
2022-02-25 13:27:36 +0300

    :todo: cli tool changes

---
+++ go-todo/cmd/todo/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"sonhador.ru/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	// parsing cmd line flags
+	task := flag.String("task", "", "Task to be included in todo list")
+	list := flag.Bool("list", false, "Liast all tasks")
+	complete := flag.Int("complete", 0, "ITem to be completed")
+	flag.Parse()
+
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+	case *task != "":
+		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
